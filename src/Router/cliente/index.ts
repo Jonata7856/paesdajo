0617c002e19c8c2318b3f2977b836363ea6c8625
Jonatas Orestes <jonatasorestes7856@gmail.com>
2023-04-11 11:59:21 -0300

    ajuste na edição do cliente

--- src/Router/cliente/index.ts
+++ src/Router/cliente/index.ts
@@ -1,12 +1,12 @@
 import { Router, Request, Response } from 'express';
 import { CadastroController } from '../../controller/cliente/CadastroController';
 import { BuscaController} from '../../controller/cliente/BuscaController';
-import { EditarController} from '../../controller/produto/EditarController';
+import { EditarController} from '../../controller/cliente/EditarController';
 
 const cliente = Router();
 cliente.post("/cadastro", new CadastroController().handle)
 cliente.get("/busca", new BuscaController().handle)
 cliente.get("/busca/:id", new BuscaController().handle)
-cliente.post("/Editar", new EditarController().handle)
+cliente.post("/editar", new EditarController().handle)
 
 export default cliente;
