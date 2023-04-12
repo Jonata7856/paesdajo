94e07640bfe95b707485eae354097eee6609b060
Jonatas Orestes <jonatasorestes7856@gmail.com>
2023-04-12 20:06:38 -0300

    buscar cidade pelo id

--- src/controller/lista-cidade/idCidadeController.ts
+++ src/controller/lista-cidade/idCidadeController.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from "express";
 import prisma from "../../database/PrismaClient";
+import cidade from "../../Router/lista-cidade";
+import { log } from "console";
+
 
 export class idCidadeController {
   async handle(req: Request, res: Response): Promise<Response> {
-    let cidades;
-    if (req.params.id) {
-      cidades = await prisma.cidade.findMany({
-        where: {
-          idcidade: parseInt(req.params.id),
-        },
-      });
-    } else {
-      cidades = await prisma.cidade.findMany();
-    }
-    return res.send(cidades);
+    const idcidade = Number(req.params.idcidade);
+    const cidade = await prisma.cidade.findUnique({
+      where: {
+        idcidade: idcidade,
+      },
+    });    
+    return res.send(cidade);
   }
 }
