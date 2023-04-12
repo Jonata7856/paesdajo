fba804d3e2fdb0dac36752b96fd78a8610776c89
Jonatas Orestes <jonatasorestes7856@gmail.com>
2023-04-12 10:20:24 -0300

    busca cidade

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import cliente from './cliente';
 import produto from './produto';
+import cidade from './lista-cidade';
 
 const router = Router();
 
 router.use("/cliente", cliente)
 router.use("/produto", produto)
+router.use("/cidade", cidade)
 
 export default router;
