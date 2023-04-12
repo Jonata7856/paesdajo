9b1a4769457b989050aa952fc7025f2a4e9505d2
Jonatas Orestes <jonatasorestes7856@gmail.com>
2023-04-12 20:09:55 -0300

    buscar bairros através do idcidade

--- src/controller/bairro/BairroController.ts
+++ src/controller/bairro/BairroController.ts
@@ -1,19 +1,15 @@
+
 import { Request, Response } from "express";
 import prisma from "../../database/PrismaClient";
-import bairro from "../../Router/bairro";
 
 export class BairroController {
   async handle(req: Request, res: Response): Promise<Response> {
-    let bairro;
-    if (req.params.id) {
-      bairro = await prisma.bairro.findMany({
-        where: {
-          idcidade: parseInt(req.params.id),
-        },
-      });
-    } else {
-      bairro = await prisma.cidade.findMany();
-    }
-    return res.send(bairro);
+    const bairros = await prisma.bairro.findMany({
+      where: {
+        idcidade: parseInt(req.params.id),
+      },
+    });
+
+    return res.send(bairros);
   }
 }
