c08d14a32f1ed72313169169790cbf66d930e6fe
Jonatas Orestes <jonatasorestes7856@gmail.com>
2023-04-12 10:18:10 -0300

    busca cidade

---
+++ src/controller/lista-cidade/CidadeController.ts
@@ -0,0 +1,19 @@
+
+import { Request, Response } from "express";
+import prisma from "../../database/PrismaClient";
+
+export class CidadeController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    let cidades;
+    if (req.params.id) {
+      cidades = await prisma.cidade.findMany({
+        where: {
+          idestado: parseInt(req.params.id),
+        },
+      });
+    } else {
+      cidades = await prisma.cidade.findMany();
+    }
+    return res.send(cidades);
+  }
+}
