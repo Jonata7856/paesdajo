bc0a295569f327695fc8756df72c98ae9947e342
Jonatas Orestes <jonatasorestes7856@gmail.com>
2023-04-12 23:54:06 -0300

    editar bairro

--- src/controller/bairro/BairroController.ts
+++ src/controller/bairro/BairroController.ts
@@ -4,12 +4,13 @@ import prisma from "../../database/PrismaClient";
 
 export class BairroController {
   async handle(req: Request, res: Response): Promise<Response> {
-    const bairros = await prisma.bairro.findMany({
-      where: {
-        idcidade: parseInt(req.params.id),
-      },
+    const idcidade = Number(req.params.idcidade); 
+    console.log(req.params)
+    const bairro = await prisma.bairro.findMany({
+      where: { idcidade: Number(req.params.id) }
     });
-
-    return res.send(bairros);
+    
+    return res.send(bairro);
   }
 }
+
---
+++ src/controller/bairro/EditarController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import prismaClient from "../../database/PrismaClient";
+
+export class EditarController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { idbairro, nome, idcidade, valortele } = req.body;
+    const bairroAtualizado = await prismaClient.bairro.update({
+      where: { idbairro },
+      data: { nome, idcidade, valortele }
+    });
+
+    return res.send("Bairro atualizado com sucesso!")
+  }
+}
