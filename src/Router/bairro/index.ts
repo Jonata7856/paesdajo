98e54fb033c84d0b1f633fa50b5b68c86a2583e1
Jonatas Orestes <jonatasorestes7856@gmail.com>
2023-04-12 21:30:38 -0300

    cadastro de bairros

--- src/Router/bairro/index.ts
+++ src/Router/bairro/index.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from 'express';
-
 import { BairroController } from '../../controller/bairro/BairroController';
+import { CadastrobairroController } from '../../controller/bairro/CadastrobairroController';
 
 const  bairro = Router();
 
 bairro.get("/busque/:id", new BairroController().handle)
+bairro.post("/cadastro", new CadastrobairroController ().handle)
 
 export default bairro;
