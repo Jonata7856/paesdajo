2ca6a4d3340639230193e3d6b60356271292f2ad
Jonatas Orestes <jonatasorestes7856@gmail.com>
2023-04-13 23:50:26 -0300

    validações endereço cliente

--- src/controller/endereco/CadastroController.ts
+++ src/controller/endereco/CadastroController.ts
@@ -1,10 +1,34 @@
 import { Request, Response } from "express";
 import prisma from "../../database/PrismaClient";
-
 export class CadastroController {
   async handle(req: Request, res: Response): Promise<Response> {
     const { idcliente, endereco, numero, complemento, idbairro } = req.body;
-    await prisma.enderecocliente.create({ data: { idcliente, endereco, numero, complemento, idbairro } });
-    return res.send("Endereço cliente cadastrado com sucesso!")
+
+    // Validações
+    if (!idcliente || isNaN(idcliente)) {
+      return res.status(400).send("O campo 'idcliente' é obrigatório e deve ser um número.");
+    }
+
+    if (!endereco || endereco.trim().length === 0) {
+      return res.status(400).send("O campo 'endereco' é obrigatório.");
+    }
+
+    if (!numero || numero.trim().length === 0) {
+      return res.status(400).send("O campo 'numero' é obrigatório.");
+    }
+
+    if (idbairro && isNaN(idbairro)) {
+      return res.status(400).send("O campo 'idbairro' deve ser um número.");
+    }
+
+    try {
+      await prisma.enderecocliente.create({
+        data: { idcliente, endereco, numero, complemento, idbairro },
+      });
+      return res.send("Endereço cliente cadastrado com sucesso!");
+    } catch (error) {
+      console.error("Erro ao cadastrar endereço do cliente:", error);
+      return res.status(500).send("Erro ao cadastrar endereço do cliente.");
+    }
   }
 }
--- src/controller/endereco/EditarController.ts
+++ src/controller/endereco/EditarController.ts
@@ -4,11 +4,33 @@ import prismaClient from "../../database/PrismaClient";
 export class EditarController {
   async handle(req: Request, res: Response): Promise<Response> {
     const { idenderecocliente, idcliente, numero, complemento, idbairro } = req.body;
-    const enderecoAtualizado = await prismaClient.enderecocliente.update({
-      where: { idenderecocliente },
-      data: { idcliente, numero, complemento, idbairro }
-    });
 
-    return res.send("Endereço do cliente atualizado com sucesso!")
+    // Validações
+    if (!idenderecocliente || isNaN(idenderecocliente)) {
+      return res.status(400).send("O campo 'idenderecocliente' é obrigatório e deve ser um número.");
+    }
+
+    if (!idcliente || isNaN(idcliente)) {
+      return res.status(400).send("O campo 'idcliente' é obrigatório e deve ser um número.");
+    }
+
+    if (!numero || numero.trim().length === 0) {
+      return res.status(400).send("O campo 'numero' é obrigatório.");
+    }
+
+    if (idbairro && isNaN(idbairro)) {
+      return res.status(400).send("O campo 'idbairro' deve ser um número.");
+    }
+
+    try {
+      const enderecoAtualizado = await prismaClient.enderecocliente.update({
+        where: { idenderecocliente },
+        data: { idcliente, numero, complemento, idbairro },
+      });
+      return res.send("Endereço do cliente atualizado com sucesso!");
+    } catch (error) {
+      console.error("Erro ao atualizar endereço do cliente:", error);
+      return res.status(500).send("Erro ao atualizar endereço do cliente.");
+    }
   }
 }
