3ae0fe9f14f7806e84b9e09234399cf8a8020fa3
Jonatas Orestes <jonatasorestes7856@gmail.com>
2023-04-13 23:58:32 -0300

    renomeação da pasta cidade

---
+++ src/controller/cidade/idCidadeController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import prisma from "../../database/PrismaClient";
+import cidade from "../../Router/cidade";
+import { log } from "console";
+
+
+export class idCidadeController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const idcidade = Number(req.params.idcidade);
+    const cidade = await prisma.cidade.findUnique({
+      where: {
+        idcidade: idcidade,
+      },
+    });    
+    return res.send(cidade);
+  }
+}
